370b559f6a5024f856d89be5e763790c4b568402
Bilei <bilei121@outlook.com>
2021-02-13 19:06:07 +0800

    Update hybrid-dual-estimator.py

--- hybrid-dual-estimator.py
+++ hybrid-dual-estimator.py
@@ -22,6 +22,9 @@ Frodo1344={'series':'Frodo','name':1344,'parameter':{'n':1344,'q':2**16,'s':1.4,
 NTRULPrime653={'series':'NTRULPrime','name':653,'parameter':{'n':653,'q':4621,'s':(2/3)**0.5,'mm':909,'c':1.3},'distribution':{0:401/653,1:252/653/2}}
 NTRULPrime761={'series':'NTRULPrime','name':761,'parameter':{'n':761,'q':4591,'s':(2/3)**0.5,'mm':1017,'c':1.4},'distribution':{0:511/761,1:250/761/2}}
 NTRULPrime857={'series':'NTRULPrime','name':857,'parameter':{'n':857,'q':5167,'s':(2/3)**0.5,'mm':1113,'c':1.4},'distribution':{0:576/857,1:281/857/2}}
+NTRULPrime953={'series':'NTRULPrime','name':953,'parameter':{'n':953,'q':6343,'s':(2/3)**0.5,'mm':1209,'c':1.4},'distribution':{0:608/953,1:345/953/2}}
+NTRULPrime1013={'series':'NTRULPrime','name':1013,'parameter':{'n':1013,'q':7177,'s':(2/3)**0.5,'mm':1269,'c':1.3},'distribution':{0:621/1013,1:392/1013/2}}
+NTRULPrime1277={'series':'NTRULPrime','name':1277,'parameter':{'n':1277,'q':7879,'s':(2/3)**0.5,'mm':1533,'c':1.4},'distribution':{0:848/1277,1:429/1277/2}}
 
 kappa=128 # security parameter
 
@@ -238,23 +241,29 @@ def dual_guess_estimation(instance,costmodel):
 
 costmodel=1
 
-Kyber512_optimalguess=dual_guess_estimation(Kyber512,costmodel)
-Kyber768_optimalguess=dual_guess_estimation(Kyber768,costmodel)
-Kyber1024_optimalguess=dual_guess_estimation(Kyber1024,costmodel)
+Kyber512_attack=dual_guess_estimation(Kyber512,costmodel)
+Kyber768_attack=dual_guess_estimation(Kyber768,costmodel)
+Kyber1024_attack=dual_guess_estimation(Kyber1024,costmodel)
 
-Saber512_optimalguess=dual_guess_estimation(Saber512,costmodel)
-Saber768_optimalguess=dual_guess_estimation(Saber768,costmodel)
-Saber1024_optimalguess=dual_guess_estimation(Saber1024,costmodel)
+Saber512_attack=dual_guess_estimation(Saber512,costmodel)
+Saber768_attack=dual_guess_estimation(Saber768,costmodel)
+Saber1024_attack=dual_guess_estimation(Saber1024,costmodel)
 
-Dilithium768_optimalguess=dual_guess_estimation(Dilithium768,costmodel)
-Dilithium1024_optimalguess=dual_guess_estimation(Dilithium1024,costmodel)
-Dilithium1280_optimalguess=dual_guess_estimation(Dilithium1280,costmodel)
+Dilithium768_attack=dual_guess_estimation(Dilithium768,costmodel)
+Dilithium1024_attack=dual_guess_estimation(Dilithium1024,costmodel)
+Dilithium1280_attack=dual_guess_estimation(Dilithium1280,costmodel)
 
-Frodo640_optimalguess=dual_guess_estimation(Frodo640,costmodel)
-Frodo976_optimalguess=dual_guess_estimation(Frodo976,costmodel)
-Frodo1344_optimalguess=dual_guess_estimation(Frodo1344,costmodel)
+Frodo640_attack=dual_guess_estimation(Frodo640,costmodel)
+Frodo976_attack=dual_guess_estimation(Frodo976,costmodel)
+Frodo1344_attack=dual_guess_estimation(Frodo1344,costmodel)
 
-NTRULPrime653_optimalguess=dual_guess_estimation(NTRULPrime653,costmodel)
-NTRULPrime761_optimalguess=dual_guess_estimation(NTRULPrime761,costmodel)
-NTRULPrime857_optimalguess=dual_guess_estimation(NTRULPrime857,costmodel)
+NTRULPrime653_attack=dual_guess_estimation(NTRULPrime653,costmodel)
+NTRULPrime761_attack=dual_guess_estimation(NTRULPrime761,costmodel)
+NTRULPrime857_attack=dual_guess_estimation(NTRULPrime857,costmodel)
+NTRULPrime953_attack=dual_guess_estimation(NTRULPrime953,costmodel)
+NTRULPrime1013_attack=dual_guess_estimation(NTRULPrime1013,costmodel)
+NTRULPrime1277_attack=dual_guess_estimation(NTRULPrime1277,costmodel)
+
+
+
 
